278f51df5774e04ca0da44d0b350cdc70dc5271d
Het Patel <hetexperiment28@gmail.com>
2026-06-22 01:41:33 +0530

    day2

---
+++ problems.py
@@ -0,0 +1,38 @@
+# # q1
+# a = 6
+# b = 5
+# print(a+b)
+
+# q2
+# a = 54
+
+# b = 5
+
+# print("remender is:", a % b )
+
+# q3
+# a = input("enter value:")
+# print(type(a))
+
+# or this also works 
+
+# a = input("enter value:")
+# t = type(a)
+# print(t)
+
+# q4
+# a = input("enter no.1 ")
+# b = input("enter no.2")
+
+# print("answer of given no. if a greater than b is", a > b )
+
+# q5
+# a = int(input("enter no.1 "))
+# b = int(input("enter no.2"))
+# print("average of 2 no. is :", (a+b)/2 )
+
+# q6
+a = int(input("enter no.1 "))
+# print("square of 2no. is :", a**2 )
+# or
+# print("square of 2no. is :", a*a )
