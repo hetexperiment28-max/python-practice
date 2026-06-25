c6af235927232c23366d5e7f5840e70964488980
Het Patel <hetexperiment28@gmail.com>
2026-06-26 01:36:17 +0530

    Add files via upload

---
+++ day6_conditionals.py
@@ -0,0 +1,15 @@
+a = int(input("enter age: "))
+#if elif else Ladder
+if(a>=18):
+    print("you are above the age for consent")
+
+elif(a<0):
+    print("you enter wrong age")
+
+else:
+    print("you are not eligible for consent:")
+
+print("end program")
+
+
+
