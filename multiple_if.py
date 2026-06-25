7bee39d5447d5fb0f956a3a4f64a30ea171bdb77
Het Patel <hetexperiment28@gmail.com>
2026-06-26 01:36:38 +0530

    Add files via upload

---
+++ multiple_if.py
@@ -0,0 +1,17 @@
+a = int(input("enter age: "))
+
+if(a%2 == 0):
+
+    print("a is even")
+#end of if statement 2
+
+if(a>=18):
+    print("you are above the age for consent")
+
+elif(a<0):
+    print("you enter wrong age")
+
+else:
+    print("you are not eligible for consent:")
+
+print("end program")
