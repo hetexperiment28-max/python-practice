78e0f64e1ff19d1f49512d03e829f24d9f099568
Het Patel <hetexperiment28@gmail.com>
2026-07-01 00:49:18 +0530

    Add files via upload

---
+++ day10_class.py
@@ -0,0 +1,11 @@
+class employee:
+    language = "Python"
+    salary = 1000
+
+het = employee ()
+het.name = "het patel"
+print(het.name, het.salary, het.language)
+
+harry = employee ()
+harry.name = "harry code"
+print(harry.name ,het.salary, het.language)
